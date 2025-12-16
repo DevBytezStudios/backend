8fedbfe606ed90f4562a5d17d54d3685ed33f145
Davi Souza <daviekalebe2200@gmail.com>
2025-12-16 09:39:18 -0300

    Fixing bugs

--- app/Http/Controllers/Dashboard/ConfeitariaController.php
+++ app/Http/Controllers/Dashboard/ConfeitariaController.php
@@ -16,39 +16,45 @@ class ConfeitariaController extends Controller
 {
     public function setInfo(Request $request)
     {
-       try{
-         $data = json_decode($request->data, true);
+        try {
+            $data = json_decode($request->data, true);
 
-        $logo = ' ';
-        if ($request->logo) {
-            $path = Storage::disk('public')->put('confeitarias', $request->logo);
-            $logo = basename($path);
-        } else {
-            $logo = "semImagem.jpg";
-        }
+            if ($request->logo) {
+                $logo = ' ';
+                if ($request->logo) {
+                    $path = Storage::disk('public')->put('confeitarias', $request->logo);
+                    $logo = basename($path);
+                } else {
+                    $logo = "semImagem.jpg";
+                }
+            }
 
 
-        $confeitaria = Auth::user();
-        $confeitaria = Confeitaria::find($confeitaria->id);
-        $confeitaria->nome = $data['confeitaria']['nome'];
-        $confeitaria->cor_princ = $data['confeitaria']['cor_princ'];
-        $confeitaria->cor_sec = $data['confeitaria']['cor_sec'];
-        $confeitaria->logo = $logo;
-        $confeitaria->save();
-        return [
-            'success' => [
-                'titulo' => 'Atualizada!',
-                'confeitaria' => $confeitaria
-            ]
-        ];
-       }catch(Throwable $error){
-         return [
+
+            $confeitaria = Auth::user();
+            $confeitaria = Confeitaria::find($confeitaria->id);
+            $confeitaria->nome = $data['confeitaria']['nome'];
+            $confeitaria->cor_princ = $data['confeitaria']['cor_princ'];
+            $confeitaria->cor_sec = $data['confeitaria']['cor_sec'];
+
+            if ($request->logo) {
+                $confeitaria->logo = $logo;
+            }
+            $confeitaria->save();
+            return [
+                'success' => [
+                    'titulo' => 'Atualizada!',
+                    'confeitaria' => $confeitaria
+                ]
+            ];
+        } catch (Throwable $error) {
+            return [
                 'error' => [
                     'titulo' => 'Algo de errado!',
                     'message' => $error->getMessage(),
                     'code' => $error->getCode(),
                 ]
             ];
-       }
+        }
     }
 }
