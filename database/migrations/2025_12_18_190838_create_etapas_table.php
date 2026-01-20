01475b6856675bf8b267a13f298e6b44106b72d9
Davi Souza <daviekalebe2200@gmail.com>
2026-01-20 15:32:16 -0300

    minor fixing bugs

--- database/migrations/2025_12_18_190838_create_etapas_table.php
+++ database/migrations/2025_12_18_190838_create_etapas_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('id_con');
             $table->foreign('id_con')->references('id')->on('confeitarias')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('nome')->unique();
+            $table->string('nome');
             $table->integer('ordem');
             $table->boolean('required');
             $table->boolean('multiple');
